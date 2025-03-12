3f0a28e2247ea0cf5daa6a61bcfd52cd5456e74e
Thavornn <pinklemon122@gmail.com>
2025-03-12 23:27:19 +0700

    first commit

---
+++ src/main/java/org/example/spring_homework001/SpringHomework.java
@@ -0,0 +1,13 @@
+package org.example.spring_homework001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomework {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHomework.class, args);
+    }
+
+}
